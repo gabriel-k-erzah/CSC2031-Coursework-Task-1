309d715f447562c5d603b0fb5149a78b05bbd7ec
Gabriel Kumbutso erzah <gabrielerzah@gmail.com>
2025-11-04 13:17:23 +0000

    changed forms from a dir to a package

---
+++ task_1/app/domain/rules.py
@@ -0,0 +1,47 @@
+def username():
+    """Length: 3–30 characters
+Allowed characters: letters and underscores
+Disallow reserved usernames: admin, root, superuser"""
+    pass
+
+
+def email():
+"""Validate format
+Restrict domains. Only allow emails ending in .edu, .ac.uk, and .org"""
+    pass
+
+
+def password():
+"""Implement a robust password policy that enforces:
+
+Minimum length: 12 characters
+At least:
+One uppercase letter
+One lowercase letter
+One digit
+One special character
+Must not contain the username or email
+Must not contain whitespace characters (e.g., spaces)
+Must not be a common password in the list:
+password123
+admin
+123456
+qwerty
+letmein
+welcome
+iloveyou
+abc123
+monkey
+football"""
+    pass
+
+def confirm_password():
+
+
+def BioorCommentField():
+    pass
+
+
+#jinja2.exceptions.TemplateSyntaxError: Encountered unknown tag 'end'.
+# Jinja was looking for the following tags: 'endblock'. The innermost
+# block that needs to be closed is 'block'.
