726be30f959c3c69d0b1d85fc35f8bc2a036af3a
Gabriel <gabrielerzah@gmail.com>
2025-11-04 14:27:05 +0000

    started to write username() checks with helper methods to build on for further security

--- task_1/app/domain/rules.py
+++ task_1/app/domain/rules.py
@@ -1,10 +1,41 @@
-def username():
+
+#######
+def username(value):
     """Length: 3–30 characters
 Allowed characters: letters and underscores
 Disallow reserved usernames: admin, root, superuser"""
-    pass
+    value = str(value)
 
+    # iteration over the string to check for only letters and underscores
+    check_char(value)
+    # check for if the string entered is in range
+    check_length(value)
+    #check for name attempt and log, incase user tries to elevate
+    check_reserved(value)
 
+    return value
+
+#helper methods
+def check_char(value):
+    for char in value:
+        if not (char.isalpha() or char == "_"):
+            raise ValueError("Username can only contain letters and underscores.")
+
+def check_length(value):
+    if len(value) < 3 or len(value) > 30:
+        raise ValueError("Username must be between 3 and 30 characters.")
+    return value
+
+def check_reserved(name):
+    reserved = {"admin", "root", "superuser"}
+    name = str(name).strip().lower()
+    if name in reserved:
+        raise ValueError("Attempt logged.")
+    return name
+
+########
+
+########
 def email():
 """Validate format
 Restrict domains. Only allow emails ending in .edu, .ac.uk, and .org"""
